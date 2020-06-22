ad6f8a678690811045a1dd43e8ea761f06a5e542
m3ntru <zatd39@gmail.com>
2020-06-23 01:11:51 +0800

    version 0.1.6 (2020/6/23)
    1.新增(?)
    2.程式排版修正

--- src/Components/MainConverter/index.js
+++ src/Components/MainConverter/index.js
@@ -4,18 +4,23 @@ import Converter from '../../model/Converter'
 import logoCgua from '../../img/tgm3Cgua.png';
 import logoOD from '../../img/tgm3OD.png';
 import logoAmon from '../../img/tgm3AMON.png';
+import ONONON from '../../img/ONONON.gif';
+import HappyBirth from '../../HB.mp3';
 import { Container, Typography, Card, CardContent, Button, TextField, Paper, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core/';
 import Example from '../../example.json'
 import ReactPlayer from 'react-player'
+import Konami from 'react-konami-code';
+import AudioPlayer from 'react-audio-player';
 
 class MainConverter extends Component {
     state = {
         text: "",
         result: [],
         logo: logoCgua,
-        example : 0,
+        example: 0,
         player: false,
-        playerLink: ''
+        playerLink: '',
+        HappyBirth: false
     }
 
     handleChange = name => event => {
@@ -57,82 +62,98 @@ class MainConverter extends Component {
         }
     }
 
-render() {
-    return (
-        <div style={{ margin: '20px' }}>
-            <Container style={{ backgroundColor: '#e0f2f1', margin: '12px auto', padding: '0px' }} >
-                <div style={{ backgroundColor: '#009688', padding: '15px' }}>
-                    <img src={this.state.logo} alt='logo' />
-                </div>
-                <div>
-                    <Card style={{ margin: '20px' }}>
-                        <CardContent>
-                            <Typography variant='subtitle2' component='p'>
-                                小奇點注音斷句模擬器<br />
+    easterEgg = () => {
+        var today = new Date();
+        this.setState({
+            text: (today.getMonth() + 1) + "月" + today.getDate() + "日是我們的實況主———吳彩學的生日，記得跟彩學說聲生日快樂喔！ https://clips.twitch.tv/ClumsyProudShrewHeyGuys",
+            logo: ONONON,
+            HappyBirth: true
+        })
+    }
+
+    render() {
+        return (
+            <div style={{ margin: '20px' }}>
+                <Container style={{ backgroundColor: '#e0f2f1', margin: '12px auto', padding: '0px' }} >
+                    <div style={{ backgroundColor: '#009688', padding: '15px' }}>
+                        <img src={this.state.logo} alt='logo' />
+                    </div>
+                    <div>
+                        <Card style={{ margin: '20px' }}>
+                            <CardContent>
+                                <Typography variant='subtitle2' component='p'>
+                                    小奇點注音斷句模擬器<br />
                                 1.目前僅有純注音斷句，文字上限以及混合中文或Emoji還有待測試<br />
                                 2.中文轉注音轉換工具 <a href='https://www.chineseconverter.com/zh-tw/convert/zhuyin' target='blank'>
-                                    https://www.chineseconverter.com/zh-tw/convert/zhuyin</a><br />
+                                        https://www.chineseconverter.com/zh-tw/convert/zhuyin</a><br />
                                 3.試聽功能障礙排除中，目前試聽只能連結到google翻譯，請按右邊的聆聽按鈕。<br />
-                                <Typography variant='caption' style={{ color: '#999999' }}>
-                                    (在google翻譯，要聽舊的google姐需用翻譯簡體後的結果，但右邊的翻譯結果太長時會把空格吃掉，<br />
+                                    <Typography variant='caption' style={{ color: '#999999' }}>
+                                        (在google翻譯，要聽舊的google姐需用翻譯簡體後的結果，但右邊的翻譯結果太長時會把空格吃掉，<br />
                                     所以遇到用空格的一聲字就會爆，這時將一聲的空格換成'-'就沒問題了)<br />
-                                </Typography>
+                                    </Typography>
                                 4.斷句沒有在自己想要的位置時，可使用以下注音作停頓：<br />
                                 ㄐ˙ / ㄐ˙。 / ㄙㄟ / ㄙㄟ。<br />
                                 5.有計畫新增其他功能(ex.整段試聽)，什麼時候上線不知道，作者下班也是會想打個電動
-                                <img src={logoAmon} alt='logo' style={{ height: '32px', width: '32px', verticalAlign: 'text-bottom'}} /> 
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                    <FormControl>
-                        <InputLabel id="demo-simple-select-label">範例</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={this.state.example}
-                            onChange={this.handleChange('example')}
-                        >                        
-                        {Example.case.map((data, index) => (
-                            <MenuItem key={data.id} value={data.id}>{data.title}</MenuItem>
-                        ))}
-                        </Select>
-                    </FormControl>
-                    <Button
-                        size="small"
-                        variant='contained'
-                        onClick={this.handleLoadClick}
-                        style={{ color: 'white', backgroundColor: '#009688', margin: '20px' }}
-                    >載入</Button>
-                    <div style={{ padding: '20px' }}>
-                        <TextField component={Paper}
-                            multiline
-                            variant='outlined'
-                            rows={7}
-                            label='Message'
-                            value={this.state.text}
-                            style={{ width: '100%' }}
-                            onChange={this.handleChange('text')}
-                        />
+                                <img src={logoAmon} alt='logo' style={{ height: '32px', width: '32px', verticalAlign: 'text-bottom' }} />
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                        <FormControl>
+                            <InputLabel id="demo-simple-select-label">範例</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={this.state.example}
+                                onChange={this.handleChange('example')}
+                            >
+                                {Example.case.map((data, index) => (
+                                    <MenuItem key={data.id} value={data.id}>{data.title}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        <Button
+                            size="small"
+                            variant='contained'
+                            onClick={this.handleLoadClick}
+                            style={{ color: 'white', backgroundColor: '#009688', margin: '20px' }}
+                        >載入</Button>
+                        <div style={{ padding: '20px' }}>
+                            <TextField component={Paper}
+                                multiline
+                                variant='outlined'
+                                rows={7}
+                                label='Message'
+                                value={this.state.text}
+                                style={{ width: '100%' }}
+                                onChange={this.handleChange('text')}
+                            />
+                        </div>
+                        <Button
+                            variant='contained'
+                            onClick={this.handleConverterClick}
+                            style={{ color: 'white', backgroundColor: '#009688', margin: '20px' }}
+                        >轉換</Button>
+                        {(this.state.player) ?
+                            <ReactPlayer
+                                style={{ margin: 'auto', padding: '20px', maxWidth: '600px' }}
+                                url={this.state.playerLink}
+                                controls={true}
+                                width={'auto'}
+                            /> :
+                            ''}
+                        <ResultList result={this.state.result} />
+                        {(this.state.HappyBirth) ?
+                            <AudioPlayer
+                                src={HappyBirth}
+                                autoPlay
+                            /> :
+                            ''}
+                        <Konami action={this.easterEgg} />
                     </div>
-                    <Button
-                        variant='contained'
-                        onClick={this.handleConverterClick}
-                        style={{ color: 'white', backgroundColor: '#009688', margin: '20px' }}
-                    >轉換</Button>
-                    {(this.state.player)?
-                    <ReactPlayer 
-                        style={{ margin: 'auto', padding: '20px', maxWidth: '600px'}} 
-                        url = {this.state.playerLink}
-                        controls = {true}
-                        width = {'auto'}   
-                    />:
-                    ''} 
-                    <ResultList result={this.state.result} />
-                </div>
-            </Container>
-        </div>
-    )
-}
+                </Container>
+            </div>
+        )
+    }
 }
 
 export default MainConverter
--- src/Components/ResultList/index.js
+++ src/Components/ResultList/index.js
@@ -3,34 +3,34 @@ import { Paper, Table, TableContainer, TableCell, TableRow, TableBody, Fab } fro
 import VolumeUpIcon from '@material-ui/icons/VolumeUp';
 
 class ResultList extends React.Component {
-    render(){
+    render() {
         const TTS = 'https://translate.google.com.tw/?hl=zh-TW&tab=wT&authuser=0#view=home&op=translate&sl=zh-CN&tl=zh-CN&text=';
         // const TTS1 = 'http://translate.google.com/translate_tts?ie=UTF-8&textlen=';
         // const TTS2 = '&client=tw-ob&q=';
         // const TTS3 = '&tl=zh-CN';
 
         const { result } = this.props
-        return(
+        return (
             <div>
                 <TableContainer component={Paper}>
                     <Table aria-label="simple table">
                         <TableBody>
-                        {result.map((data, index) => (
-                            <TableRow key={index}>
-                            <TableCell width="40px">{index + 1}</TableCell>
-                            <TableCell>{data}</TableCell>
-                            <TableCell width="50px">
-                                <Fab size="medium" aria-label="Play" href={TTS + data} target='_blank'>
-                                {/* <Fab size="medium" aria-label="Play" href={TTS1.concat(90, TTS2, encodeURIComponent(data), TTS3)} target='blank'> */}
-                                    <VolumeUpIcon/>
-                                </Fab> 
-                            </TableCell>
-                            </TableRow>
-                        ))}
+                            {result.map((data, index) => (
+                                <TableRow key={index}>
+                                    <TableCell width="40px">{index + 1}</TableCell>
+                                    <TableCell>{data}</TableCell>
+                                    <TableCell width="50px">
+                                        <Fab size="medium" aria-label="Play" href={TTS + data} target='_blank'>
+                                            {/* <Fab size="medium" aria-label="Play" href={TTS1.concat(90, TTS2, encodeURIComponent(data), TTS3)} target='blank'> */}
+                                            <VolumeUpIcon />
+                                        </Fab>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
                         </TableBody>
                     </Table>
                 </TableContainer>
-        </div>
+            </div>
         )
     }
 }
